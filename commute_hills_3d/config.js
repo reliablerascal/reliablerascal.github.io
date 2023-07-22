d018c0da3b9670a4fa053bbae8b93df7b79b50de
Rob Reid <reliablerascal@gmail.com>
2023-07-22 18:59:56 -0400

    Update config.js

--- commute_hills_3d/config.js
+++ commute_hills_3d/config.js
@@ -16,36 +16,6 @@ var config = {
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/mapbox/storytelling" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
         {
-            id: 'slug-style-id',
-            alignment: 'left',
-            hidden: false,
-            title: 'Awn Broadway',
-            image: './images/riverside_bridge_near.jpeg',
-            description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-            location: {
-                
-            },
-            mapAnimation: 'easeTo',
-            rotateAnimation: false,
-            callback: '',
-            onChapterEnter: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 1,
-                //     duration: 5000
-                // }
-            ],
-            onChapterExit: [
-                // {
-                //     layer: 'layer-name',
-                //     opacity: 0
-                // }
-            ]
-        },
-
-
-
-        {
             id: 'intro',
             alignment: 'full',
             hidden: false,
@@ -53,7 +23,6 @@ var config = {
             image: './images/riverside_bridge_near.jpeg',
             description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
             location: {
-                
             },
             mapAnimation: 'easeTo',
             rotateAnimation: false,
@@ -79,14 +48,14 @@ var config = {
             id: 'broadway_155',
             alignment: 'left',
             hidden: false,
-            title: 'drive 0',
+            title: 'Broadway and 155th',
             image: './images/broadway_155th.jpeg',
             description: 'As you can see.',
             location: {
                 center: [-73.94594, 40.83288],
                 zoom: 16.45,
                 pitch: 0.00,
-                bearing: 28.00
+                bearing: 28.00,
                 // flyTo additional controls-
                 // These options control the flight curve, making it move
                 // slowly and zoom out almost completely before starting
@@ -107,7 +76,7 @@ var config = {
             id: 'broadway_133',
             alignment: 'left',
             hidden: false,
-            title: '133rd',
+            title: 'Broadway and 133rd',
             image: './images/broadway_133rd.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -129,7 +98,7 @@ var config = {
             id: 'broadway_131_view',
             alignment: 'left',
             hidden: false,
-            title: '133rd',
+            title: 'View to the East from Broadway and 131st',
             image: './images/broadway_131st_view_east.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -151,7 +120,7 @@ var config = {
             id: 'broadway_125',
             alignment: 'left',
             hidden: false,
-            title: 'Third Title',
+            title: 'Broadway and 125th',
             image: './images/broadway_125th.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -173,7 +142,7 @@ var config = {
             id: 'broadway_116',
             alignment: 'full',
             hidden: false,
-            title: 'Third Title',
+            title: 'Broadway and 116th',
             image: './images/broadway_116th.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -187,7 +156,7 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
+        },
 
 
 
@@ -195,7 +164,7 @@ var config = {
             id: 'riverside_149',
             alignment: 'full',
             hidden: false,
-            title: 'Third Title',
+            title: 'The Riverside Alternative',
             image: './images/riverside_149th.jpeg',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -205,7 +174,27 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
+        },
+
+
+
+        {
+            id: 'riverside_149',
+            alignment: 'full',
+            hidden: false,
+            title: 'A Bridge for Your Convenience',
+            image: './images/riverside_bridge_near.jpeg',
+            description: 'Copy these sections to add to your story.',
+            location: {
+            },
+            mapAnimation: 'easeTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        
+            
 
 
 
@@ -213,7 +202,7 @@ var config = {
             id: 'broadway_elevation',
             alignment: 'full',
             hidden: false,
-            title: 'Third Title',
+            title: 'A Bridge for Your Convenience',
             image: './images/broadway_elevation.svg',
             description: 'Copy these sections to add to your story.',
             location: {
@@ -223,7 +212,7 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
+        },
 
 
 
@@ -241,7 +230,7 @@ var config = {
             callback: '',
             onChapterEnter: [],
             onChapterExit: []
-        }
+        },
 
 
 
