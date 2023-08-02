a5320c38283d59ee5b1f47108787d2b3960b647d
Rob Reid <reliablerascal@gmail.com>
2023-08-02 18:42:33 -0400

    Update config.js

--- fort-washington/config.js
+++ fort-washington/config.js
@@ -1,10 +1,10 @@
 var config = {
-    //style: 'mapbox://styles/mapbox/streets-v11',
-    //accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrdTE0ZXQ0MDllZDNtbzhhdWh2MXFneiJ9.vZY4_YLk_rxN-Wc1eE7_wQ',
+    style: 'mapbox://styles/mapbox/streets-v11',
+    accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrdTE0ZXQ0MDllZDNtbzhhdWh2MXFneiJ9.vZY4_YLk_rxN-Wc1eE7_wQ',
 
     //my custom style and mapbox token
-    style: 'mapbox://styles/robcat26/clku8rcck001l01qgen1j4j2h',
-    accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrZTBrejIwMHp0YjNmcXZiaWN1enZwNiJ9._g79-Ecyuo5ahLVN3pyZZw',
+    //style: 'mapbox://styles/robcat26/clku8rcck001l01qgen1j4j2h',
+    //accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrZTBrejIwMHp0YjNmcXZiaWN1enZwNiJ9._g79-Ecyuo5ahLVN3pyZZw',
 
     showMarkers: true,
     markerColor: '#3FB1CE',
@@ -43,10 +43,10 @@ var config = {
             rotateAnimation: false,
             callback: '',
             onChapterEnter: [
-                {
-                layer: 'fort-washington-park-access',
-                opacity:1
-            }
+            //     {
+            //     layer: 'fort-washington-park-access',
+            //     opacity:1
+            // }
         ],
             onChapterExit: []
         },
