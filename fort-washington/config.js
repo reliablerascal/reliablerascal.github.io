a59a2aaf9dee06c340dcfbbcfa19983268969dde
Rob Reid <reliablerascal@gmail.com>
2023-08-04 11:40:07 -0400

    finished Fort Washington narrative

--- fort-washington/config.js
+++ fort-washington/config.js
@@ -28,7 +28,7 @@ var config = {
             hidden: false,
             //title: '',
             image: './images/001_fort_washington_park-1280-960.jpg',
-            //description: 'Nestled between the Hudson River and the ceaseless flow of automotive traffic on the Henry Hudson Expressway, Fort Washington Park offers a setting for both social gatherings and solitary escapes from the concrete urban expanse above cliffs to the east. Barbecuers, basketball players, bicyclists, and joggers occupy the dirt paths, paved trails, atheletic courts, and lawn.',
+            //description: 'Nestled between the Hudson River and the ceaseless flow of automotive traffic on the Henry Hudson Expressway, Fort Washington Park offers a setting for both social gatherings and solitary escapes from the concrete urban expanse above cliffs to the east. Barbecuers, basketball players, bicyclists, and joggers occupy the park'sdirt paths, paved trails, atheletic courts, and lawn.',
             //and the The Oxford English dictionary defines a fort as a person or thing not susceptible to outside influence or disturbance.',
             location: {
                 center: [-73.94616, 40.84513],
@@ -63,7 +63,7 @@ var config = {
             hidden: false,
             //title: '',
             //image: './images/001_fort_washington_park.jpeg',
-            description: 'Nestled between the Hudson River and the ceaseless flow of automotive traffic on the Henry Hudson Expressway, Fort Washington Park offers both social gathering spaces and solitary escapes from the concrete urban jungle above cliffs to the east. Barbecuers, basketball players, bicyclists, and pedestrians occupy the dirt paths, paved trails, athletic courts, and rocky river bank.',
+            description: 'Nestled between the Hudson River and the ceaseless flow of automotive traffic on the Henry Hudson Expressway, Fort Washington Park offers both social gathering spaces and solitary escapes from the concrete urban jungle above cliffs to the east. Barbecuers, basketball players, bicyclists, and pedestrians share the park\'s dirt paths, paved trails, athletic courts, and rocky river bank.',
             //and the The Oxford English dictionary defines a fort as a person or thing not susceptible to outside influence or disturbance.',
             location: {
                 center: [-73.94616, 40.84513],
@@ -360,8 +360,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             //title: '',
-            //image: './images/007a_alltrails_182_gap.png',
-            description: 'North of the George Washington Bridge, the riverfront trail turns inland. Crossing over the Amtrak and under the expressway, it connects to a narrow sidewalk on Riverside Drive alongside traffic.',
+            //image: './images/006b_182nd_crossing.jpg',
+            description: 'North of the George Washington Bridge, the riverfront trail turns inland. Crossing over the Amtrak, under the the expressway, it connects to a narrow sidewalk on Riverside Drive alongside traffic.',
             location: {
                 center: [-73.94161, 40.85336],
                 zoom: 15.07,
@@ -382,7 +382,7 @@ var config = {
             hidden: false,
             //title: '',
             image: './images/007a_alltrails_182_gap.png',
-            description: 'It\'s not uncommon for Google Maps to exclude or obscure pedestrian walking paths while highlighting automotive routes. But popular hiking app <a href="https://www.alltrails.com/" target="_blank">Alltrails</a>, built on the open-source <a href="https://www.openstreetmap.org/#map=4/38.01/-95.84">Open Street Map</a>, confirms that the official riverside trail terminates north of the bridge.',
+            description: 'It\'s not uncommon for online maps to exclude or obscure pedestrian walking paths while highlighting automotive routes. Popular hiking app <a href="https://www.alltrails.com/" target="_blank">Alltrails</a>, built on the open-source <a href="https://www.openstreetmap.org/#map=4/38.01/-95.84">Open Street Map</a>, confirms that the official riverside trail terminates north of the bridge.',
             location: {
                 center: [-73.94161, 40.85336],
                 zoom: 15.07,
@@ -441,12 +441,12 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'C-social-trail-about',
+            id: 'C-social-trail-entry',
             alignment: 'left',
             hidden: false,
             //title: '',
-            //image: './images/002_topographical_history.jpg',
-            description: 'show it, describe what a social trail is.',
+            image: './images/009_social_entry_south.jpeg',
+            description: 'Once I knew that a riverfront trail existed, it was easy to notice the trail spur alongside the fence just before the bridge crossing the train tracks.',
             location: {
                 center: [-73.94161, 40.85336],
                 zoom: 15.07,
@@ -463,12 +463,12 @@ var config = {
             onChapterExit: []
         },
         {
-            id: 'C-social-trail-entry',
+            id: 'C-social-doc',
             alignment: 'left',
             hidden: false,
             //title: '',
-            image: './images/009_social_entry_south.jpeg',
-            description: '“As I went walking I saw a sign there,<br>And on the sign it said "No Trespassing."<br>But on the other side it didn\'t say nothing.<br>That side was made for you and me.”<br>-Arlo Guthrie.',
+            image: './images/010_social_dock.jpg',
+            description: 'Though I encountered only one other person on the trail, I saw clear signs that it\'d been used extensively. This makeshift dock suggests an ambitious fishing endeavor.',
             location: {
                 center: [-73.94161, 40.85336],
                 zoom: 15.07,
@@ -489,8 +489,30 @@ var config = {
             alignment: 'left',
             hidden: false,
             //title: '',
-            image: './images/010_social_riverside.jpeg',
-            description: 'Direct conflict with train, pinched between trail and river, estimate to extend trail was $22 million.',
+            image: './images/011_social_riverside.jpeg',
+            description: 'Pinched between the Hudson and the fence barricading Amtrak\'s rails, the trail offered unobstructed views of the river, the George Washington Bridge, and the New Jersey Palisades. But it\'s clearly not set up to support heavy traffic. Railroad ballast supporting some especially narrow stretches of the trail threatens to break up into the river.',
+            location: {
+                center: [-73.94161, 40.85336],
+                zoom: 15.07,
+                pitch: 0.00,
+                bearing: 28.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {layer: 'path-dyckman', opacity:1},
+                {layer: 'path-social', opacity:1}
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'C-social-trail-about',
+            alignment: 'left',
+            hidden: false,
+            //title: '',
+            //image: './images/002_topographical_history.jpg',
+            description: 'Though there was no clear indication that this path is prohibited, I would later learn that the connector trail is what is known as a <em>social trail</em>. According to the National Park Service, a <a href="https://www.nps.gov/articles/000/social-trails.htm#:~:text=A%20social%20trail%20is%20an,look%20like%20an%20official%20trail.&text=To%20set%20a%20good%20example,day%2C%20stay%20on%20designated%20trails" target="_blank">social trail is an unofficial detour created by hikers</a> that gets worn down to the point of <em>appearing</em> to be an official trail.<p>The National Park Service warns that use of these trails can result in ecological damage. And park stewards note that <a href="https://www.reddit.com/r/openstreetmap/comments/ozz4zx/comment/h84912u/?utm_source=share&utm_medium=android_app&utm_name=androidcss&utm_term=2&utm_content=share_button&rdt=48941&onetap_auto=true" target="_blank">hiding these social trails from official maps is a deliberate choice</a>- reflecting ecological concerns as well as legal public access, safety, and maintenance considerations.',
             location: {
                 center: [-73.94161, 40.85336],
                 zoom: 15.07,
@@ -511,8 +533,8 @@ var config = {
             alignment: 'left',
             hidden: false,
             //title: '',
-            image: './images/011_social_outside_rail.jpeg',
-            description: 'Ethical considerations- maintenance, etc. Why they’re not listed on maps.',
+            image: './images/012_social_outside_rail.jpeg',
+            description: 'At the same time, there\'s clear evidence that the path is widely used, and that public support could exist for developing the trail.',
             location: {
                 center: [-73.94161, 40.85336],
                 zoom: 15.07,
@@ -532,12 +554,108 @@ var config = {
             onChapterExit: []
         },
         {
+            id: 'C-social-Guthrie1',
+            alignment: 'left',
+            hidden: false,
+            //title: '',
+            image: './images/013_social_north_entry.jpg',
+            description: 'At the other end of the trail, there was a sign clearly stating, "No public access beyond this point." [NEED TO GO BACK AND GET A PHOTO OF THIS] It would seem that the trail is legal going north, but illegal going south. This calls to mind a famous song.',
+            location: {
+                center: [-73.94161, 40.85336],
+                zoom: 15.07,
+                pitch: 0.00,
+                bearing: 28.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {layer: 'path-dyckman', opacity:1},
+                {layer: 'path-social', opacity:1}
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'C-social-Guthrie2',
+            alignment: 'left',
+            hidden: false,
+            //title: '',
+            //image: './images/009_social_entry_south.jpeg',
+            description: '<div align="right"><em>As I went walking I saw a sign there,<br>And on the sign it said "No Trespassing."<br>But on the other side it didn\'t say nothing.<br>That side was made for you and me.</em><br>-Arlo Guthrie</div>',
+            location: {
+                center: [-73.94161, 40.85336],
+                zoom: 15.07,
+                pitch: 0.00,
+                bearing: 28.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {layer: 'path-dyckman', opacity:1},
+                {layer: 'path-social', opacity:1}
+            ],
+            onChapterExit: []
+        },
+        {
+            id: 'trail-plans',
+            alignment: 'left',
+            hidden: false,
+            //title: '',
+            //image: './images/014_fort_washington_park.jpeg',
+            description: 'Discussion about extending the path dates back to at least 1917, when the city <a href="https://www.forttryonparktrust.org/wp-content/uploads/2017/05/1983FortTryonPark-LPC.pdf" target="_blank">spurned John D. Rockefeller Jr.\'s conditional offer</a> of the land that\'s now Fort Tryon park. The city could not commit to honor Rockefeller\'s expectation that the park be connected to Fort Washington Park.<p>More recently, the city estimated it would cost $22 million to essentially <a href="https://www.nytimes.com/2014/08/15/nyregion/new-riverside-trail-goes-only-so-far.html" target="_blank">transform the social trail to an official trail</a>- a cost elevated by a need for landfill to create enough space for pavement along the shoreline.',
+            location: {
+                center: [-73.94161, 40.85336],
+                zoom: 15.07,
+                pitch: 0.00,
+                bearing: 28.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {layer: 'path-main', opacity:1},
+                {layer: 'path-social', opacity:1},
+                {layer: 'path-dyckman', opacity:1},
+                {layer: 'path-165th', opacity:1},
+                {layer: 'path-176th', opacity:1}
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
             id: 'conclusion',
             alignment: 'left',
             hidden: false,
             //title: '',
-            image: './images/012_fort_washington_park.jpeg',
-            description: 'Rehash the land use conflicts: highway traffic (165th), train (social trail), land acquisition (secured!), Limited access makes the park isolated…but isolation may be part of the charm.',
+            image: './images/014_fort_washington_park.jpeg',
+            description: 'To this day, competition with expressway and rail access as well as the cost of maintaining walking trails limits public access to Fort Washington Park along the Hudson shoreline. But perhaps the resulting isolation is part of the park\'s charm.',
+            location: {
+                center: [-73.94616, 40.84513],
+                zoom: 14.00,
+                pitch: 0.00,
+                bearing: 28.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [
+                {layer: 'path-main', opacity:1},
+                {layer: 'path-social', opacity:1},
+                {layer: 'path-dyckman', opacity:1},
+                {layer: 'path-165th', opacity:1},
+                {layer: 'path-176th', opacity:1}
+            ],
+            onChapterExit: [
+            ]
+        },
+        {
+            id: 'credits',
+            alignment: 'left',
+            hidden: true,
+            //title: '',
+            //image: './images/012_fort_washington_park.jpeg',
+            //description: 'Rehash the land use conflicts: highway traffic (165th), train (social trail), land acquisition (secured!), Limited access makes the park isolated…but isolation may be part of the charm.',
             location: {
                 center: [-73.94616, 40.84513],
                 zoom: 14.00,
