f32d1c86b103267f2b22a073847a71201bcb9f06
Rob Reid <reliablerascal@gmail.com>
2023-08-05 11:40:21 -0400

    added mapbox tutorial

--- fort-washington/config.js
+++ fort-washington/config.js
@@ -1,8 +1,9 @@
 var config = {
-    //style: 'mapbox://styles/mapbox/streets-v11',
-    //accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrdTE0ZXQ0MDllZDNtbzhhdWh2MXFneiJ9.vZY4_YLk_rxN-Wc1eE7_wQ',
     style: 'mapbox://styles/robcat26/clku8rcck001l01qgen1j4j2h',
     accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrZTBrejIwMHp0YjNmcXZiaWN1enZwNiJ9._g79-Ecyuo5ahLVN3pyZZw',
+//style: 'mapbox://styles/mapbox/streets-v11',
+    //accessToken: 'pk.eyJ1Ijoicm9iY2F0MjYiLCJhIjoiY2xrdTE0ZXQ0MDllZDNtbzhhdWh2MXFneiJ9.vZY4_YLk_rxN-Wc1eE7_wQ',
+    
 
     //my custom style and mapbox token
     //style: 'mapbox://styles/robcat26/clku8rcck001l01qgen1j4j2h',
